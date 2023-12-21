1b1a611b2cf7def629f098dbec46586ca5d83ce9
Julian Gonzalez <joticajulian@gmail.com>
2023-12-21 23:35:36 +0000

    nft contract

---
+++ src/assembly/KondorElementusNft.ts
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+// Kondor Elementus NFT v1.0.0
+// Julian Gonzalez (joticajulian@gmail.com)
+
+import { Nft, common } from "@koinosbox/contracts";
+
+export class KondorElementusNft extends Nft {
+  _name: string = "Kondor Elementus";
+  _symbol: string = "KONDOR-ELEMENTUS";
+  _uri: string =
+    "https://kondor-nft-api-w6enmqacja-uc.a.run.app/kondor-elementus";
+
+  /**
+   * Get name of the NFT
+   * @external
+   * @readonly
+   */
+  name(): common.str {
+    return new common.str(this._name);
+  }
+}
